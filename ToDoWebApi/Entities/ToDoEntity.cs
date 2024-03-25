f99500e7771411eb6813259625e2c576ce6ebbd7
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-03-26 00:35:51 +0300

    Tamamlandi.

---
+++ ToDoWebApi/Entities/ToDoEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoWebApi.Entities
+{
+    public class ToDoEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ ToDoWebApi/Models/AddToDoRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoWebApi.Models
+{
+    public class AddToDoRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
